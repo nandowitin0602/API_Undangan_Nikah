09b4a6c821ae4adaf7d483ce16b236fdcfdf17b8
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-01-21 21:36:24 +0700

    Tambah domain wedding react prod untuk CORS

--- API_Undangan_Nikah/Program.cs
+++ API_Undangan_Nikah/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "https://nikahan-v2.vercel.app")
+            policy.WithOrigins("http://localhost:3000", "https://nikahan-v2.vercel.app", "https://wedding-efendybumi.ikodora.com")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
